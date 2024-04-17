036cf515c09cfeb31ae4ecd760db9116041eb14b
BerlyCS <berly.mandamientos@gmail.com>
2024-04-16 19:08:31 -0500

    Cmake

---
+++ CC2/15_04/ejercicios/build/CMakeFiles/tarea.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea.dir/tarea.cpp.o"
+  "CMakeFiles/tarea.dir/tarea.cpp.o.d"
+  "tarea"
+  "tarea.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CC2/15_04/ejercicios/tarea.cpp
@@ -0,0 +1,31 @@
+#include <climits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertar(vector<int> &array, int value, int &min) 
+{
+    array.push_back(value);
+    int size=array.size();
+    if (value < min) 
+    {
+        swap(array[0], array[size-1]);
+        min=value;
+    }
+
+    /* for (int i=0;i<size;i++) */
+    /*     cout<<array[i]<<" "; */
+    /* cout<<endl; */
+}
+
+int main() {
+    vector<int> array;
+    int min=INT_MAX;
+    while (true) {
+        int value;
+        cout<<"Insertar: "; 
+        cin>>value;
+        insertar(array, value, min);
+        cout<<"Array[0]: "<<array[0]<<endl;
+    }
+}
