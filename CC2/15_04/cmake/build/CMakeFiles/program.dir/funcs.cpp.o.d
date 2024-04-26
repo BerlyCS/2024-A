a9cc660cc63b406ee6eacdda5f74a7898bad023f
Berly <berly.mandamientos@gmail.com>
2024-04-26 18:16:17 -0500

    Modificacion carpetas

--- CC2/15_04/cmake/build/CMakeFiles/program.dir/funcs.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/program.dir/funcs.cpp.o: \
- /home/lenovo/Documentos/2024-A/CC2/15_04/cmake/funcs.cpp \
- /usr/include/stdc-predef.h \
- /home/lenovo/Documentos/2024-A/CC2/15_04/cmake/funcs.hpp
