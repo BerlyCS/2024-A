5d067221f4107f6da9261c44e7e684d4040bc2bf
Berly <berly.mandamientos@gmail.com>
2024-04-29 10:48:16 -0500

    nuevos archivos: CC2/04-29-LAB/1/main.cpp 	nuevos archivos: CC2/04-29-LAB/2/main2.cpp 	nuevos archivos: CC2/04-29-LAB/3/main3.cpp 	nuevos archivos: CC2/04-29-LAB/4/main4.cpp 	nuevos archivos: DBP/04-29/images/plant1.png nuevos archivos: DBP/04-29/images/plant10.png 	nuevos archivos: DBP/04-29/images/plant11.png 	nuevos archivos: DBP/04-29/images/plant12.png 	nuevos archivos: DBP/04-29/images/plant13.png 	nuevos archivos: DBP/04-29/images/plant14.png 	nuevos archivos: DBP/04-29/images/plant2.png 	nuevos archivos: DBP/04-29/images/plant3.png 	nuevos archivos: DBP/04-29/images/plant4.png 	nuevos archivos: DBP/04-29/images/plant5.png 	nuevos archivos: DBP/04-29/images/plant6.png 	nuevos archivos: DBP/04-29/images/plant7.png 	nuevos archivos: DBP/04-29/images/plant8.png 	nuevos archivos: DBP/04-29/images/plant9.png 	nuevos archivos: DBP/04-29/index.html 	modificados:     DBP/tests/css/index.html 	modificados:     DBP/tests/css/package-lock.json 	modificados:     DBP/tests/css/package.json 	modificados:     DBP/tests/css/style.css

---
+++ CC2/04-29-LAB/3/main3.cpp
@@ -0,0 +1,19 @@
+/*3. Dado un vector con un conjunto de intervalos representados por un par de valores enteros (xi, xy), se solicita
+unir los intervalos que se superponen. Por ejemplo, si se tiene los siguientes intervalos almacenados en un
+vector [(12,20),(15,32),(45,89)], el resultado que se debe obtener es la uni ́on de intervalos resultantes: [(12,32),
+(45,89)] . Todos almacenados en un vector de intervalos.*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void fun3(vector<vector<int>> test) {
+    for (int i=0; i<test.size()-1; i++) {
+            
+    }
+}
+
+
+int main() {
+    vector<vector<int>> test = {{12,20},{15,32},{45,89}};
+    fun3(test);
+}
