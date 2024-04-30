f375c9ec93c1b7ff62d76da6bc39c121390fc928
Berly <berly.mandamientos@gmail.com>
2024-04-29 22:12:37 -0500

    Clase teoria CC2 polimorfismo
    Mas informacion en cplusplus reference

---
+++ CC2/04-29/polimorfismo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    Polygon (int a, int b) : width(a), height(b) {}
+    virtual int area (void) =0;
+    void printarea()
+      { cout << this->area() << '\n'; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    Rectangle(int a,int b) : Polygon(a,b) {}
+    int area()
+      { return width*height; }
+};
+
+class Triangle: public Polygon {
+  public:
+    Triangle(int a,int b) : Polygon(a,b) {}
+    int area()
+      { return width*height/2; }
+};
+
+int main() {
+    Polygon** p= new Polygon*[5];
+    p[0] = new Rectangle(5,4);
+    p[0]->printarea();
+
+}
