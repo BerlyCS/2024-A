8ebbf62fd75f8766277ef982393a88d01a02891b
Berly <berly.mandamientos@gmail.com>
2024-05-02 18:41:22 -0500

    Lab 4.1

--- CC2/05-02-LAB/main.cpp
+++ CC2/05-02-LAB/main.cpp
@@ -1,18 +1,20 @@
+#include <bits/chrono.h>
 #include <iostream>
 #include <vector>
+#include <chrono>
 using namespace std;
 
 void print(vector<vector<vector<int>>> d) {
     for (int i=0;i<1000;i++)
         for (int j=0; j<1000;j++) 
             for (int k=0;k<3;k++) 
-                cout << tmp[i][j][k] << " ";
+                cout << d[i][j][k] << " ";
 } 
 void print(int*** d) {
     for (int i=0;i<1000;i++)
         for (int j=0; j<1000;j++) 
             for (int k=0;k<3;k++) 
-                cout << tmp[i][j][k] << " ";
+                cout << d[i][j][k] << " ";
 } 
 
 vector<vector<vector<int>>> fun1(int x=1000, int y=1000, int z=3) {
@@ -66,6 +68,19 @@ int*** fun2() {
 
 int*** suma_mat(int ***a, int***b) {
     int*** c;
+    c = new int**[1000];
+    for (int i=0;i<1000;i++) {
+        c[i] = new int*[1000];
+    }
+
+    for (int i=0;i<1000;i++) {
+        for (int j=0; j<1000;j++ ){
+            c[i][j] = new int[3];
+            for (int k=0;k<3;k++) {
+                c[i][j][k] = 0;
+            }
+        }
+    }
 
     for (int i=0;i<1000;i++) {
         for (int j=0; j<1000;j++ ){
@@ -74,7 +89,7 @@ int*** suma_mat(int ***a, int***b) {
             }
         }
     }
-
+    return c;
 }
 
 void delete_cube(int *** cube) {
@@ -92,14 +107,22 @@ void delete_cube(int *** cube) {
 }
 
 int main() {
+    int resp = -1;
+    auto start = std::chrono::high_resolution_clock::now();
 /* vector<vector<vector<int>>> vect(5, vector<vector<int>>(6, vector<int>(5, 0))) */ 
     vector<vector<vector<int>>> test1 = fun1();
     /* print(test1); */
     vector<vector<vector<int>>> test2 = fun1();
     /* print(test2); */
     vector<vector<vector<int>>> suma = suma_mat(test1, test2);
-    print(suma);
+    /* print(suma); */
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop-start);
+    cout<< resp << " for " << duration.count() << " ms" << endl;
 
-    int*** a = fun2();
-    int*** b = fun2();
+    /* int*** a = fun2(); */
+    /* int*** b = fun2(); */
+    /* int *** c = suma_mat(a,b); */
+    /* print(c); */
+
 }
