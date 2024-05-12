34be232c41c7dcc0db7e0f85489a53438e05193b
BerlyCS <berly.mandamientos@gmail.com>
2024-05-12 12:17:27 -0500

    Añadido: Arbol binario generico Lab 5 CC2

--- CC2/Lab5/main2.cpp
+++ CC2/Lab5/main2.cpp
@@ -2,3 +2,59 @@
 presentar problemas en tiempo de ejecuci´on. (se debe trabajar sin problemas con los datos int, float, double,
 long y char ). Almacene los nodos hijos en un vector. Incluya una funci´on para mostrar en pantalla el ´arbol
 de la mejor forma.*/
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+template<typename T>
+struct nodo {
+    T value;
+    nodo<T>* right;
+    nodo<T>* left;
+
+    nodo<T> (T val) : value(val), right(NULL), left(nullptr) {}
+};
+
+template<typename T>
+void insertar(nodo<T>*& raiz, T val) {
+    if (raiz == nullptr) {
+        raiz = new nodo<T>(val);
+    }
+
+    else if (val < raiz->value ) {
+        insertar(raiz->left, val);
+    }
+    else {
+        insertar(raiz->right, val);
+    }
+}
+
+template<typename T>
+void InOrden(nodo<T>* raiz) {
+    if (raiz != nullptr) {
+        InOrden(raiz->left);
+        cout << raiz->value << ' ';
+        InOrden(raiz->right);
+    }
+}
+
+int main() {
+    nodo<int>* itree = new nodo<int>(5);
+    nodo<float>* ftree = new nodo<float>(1.9);
+    nodo<char>* ctree = new nodo<char>('c');
+    nodo<long>* ltree = new nodo<long>(1);
+    nodo<double>* dtree = new nodo<double>(1.0);
+
+    for (int i=0; i<10; i++) {
+        insertar(itree, rand()%1000);
+        insertar(ftree, static_cast<float>(rand()/473382.0));
+        insertar(ctree, char(rand()%26+97));
+        insertar(ltree, static_cast<long>(rand()%1000));
+        insertar(dtree, static_cast<double>(rand()/482342.4));
+    }
+    InOrden(itree);
+    InOrden(ctree);
+    InOrden(ftree);
+    InOrden(ltree);
+    InOrden(dtree);
+}
