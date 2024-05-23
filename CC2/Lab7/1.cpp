c836d70847c87743ca3f2593ad8913aafb2960aa
BerlyCS <berly.mandamientos@gmail.com>
2024-05-23 18:46:43 -0500

    Añadido: Ejercicio 1 y 2 Lab 7 CC2
    ejercicio 1 completo, ejercicio 2 por implementar (10%)

---
+++ CC2/Lab7/1.cpp
@@ -0,0 +1,51 @@
+/*
+1. Sobrecargue el operador coma para permitir inicializar un vector<T> con n valores. Por ejemplo:
+Mylist<int> myList;
+myList, 10, 20,30,40,50,60,70,1,2,3,6,7,8,9;
+Debe de iniciar un vector<int> con cada uno de los valores separados en comas.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename X>
+class vect {
+    private:
+        X *data;
+        unsigned int size;
+    public:
+        vect<X>() {
+            size = 0;
+            data = new X[size];
+        }
+        void push_back(X val) {
+            X* tmp = new X[size+1];
+            for (int i=0; i<size; i++) 
+                tmp[i] = data[i];
+
+            tmp[size] = val;
+            delete[  ] data;
+            data = tmp;
+            size++;
+        }
+        void print() const {
+            for (int i=0; i<size; i++) {
+                cout<< data[i] << ' ';
+            }
+            cout<< endl;
+        }
+};
+
+template <typename X>
+vect<X>& operator,(vect<X>& a, X val) {
+    a.push_back(val);
+    return a;
+}
+
+int main() {
+    vect<int> a;
+    a, 10, 20, 30, 40, 50, 7, 8, 9, 3, 5 ,6 ,7;
+
+    a.print();
+}
---
+++ CC2/Lab7/2.cpp
@@ -0,0 +1,21 @@
+/*
+2. Defina el tipo de dato fraccion, e implemente los operadores suma(+), resta(-), multiplicacion(*) y division(/).
+Todas las operaciones deben realizarse como fraccion.
+*/
+#include <iostream>
+using namespace std;
+
+class fraccion {
+    private:
+        int num, den;
+    public:
+        fraccion(int a, int b):num(a),den(b) {}
+        fraccion operator+(fraccion sumando1, fraccion sumando2) {
+            fraccion result;
+            result.num = sumando1.num +
+
+        }
+};
+int main() {
+
+}
