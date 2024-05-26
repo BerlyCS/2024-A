deabbcee34a63b8592476c5111842e137094f313
BerlyCS <berly.mandamientos@gmail.com>
2024-05-25 22:40:41 -0500

    Cambios: Tarea de javascript terminada

--- DBP/Lab-JavaScript/tarea-js/js/funcs.js
+++ DBP/Lab-JavaScript/tarea-js/js/funcs.js
@@ -1,5 +1,14 @@
 function escribir() {
-    
-    document.getElementsById('namePlace').value;
-    return false;
+    let name = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let CUI = document.getElementById('CUI').value;
+    let ciudad = document.getElementById('region').value;
+    console.log(name);
+    console.log(apellido);
+    console.log(CUI);
+    console.log(ciudad);
+    alert("...");
+
+    document.getElementById('namePlace').innerHTML = name;
+    document.getElementById('apellidoPlace').innerHTML = apellido;
 }
