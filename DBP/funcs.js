e2e98c474865f6cdf2a0f1e62db726c8e4d4dc8f
BerlyCS <berly.mandamientos@gmail.com>
2024-05-25 22:43:17 -0500

    Añadido: Tests con javascript

---
+++ DBP/funcs.js
@@ -0,0 +1,12 @@
+function division(a, b = 1) {
+    return a/b;
+}
+
+let a = division(3);
+console.log(a);
+// console.clear();
+console.warn("Warn");
+console.error("Error");
+prompt("Si");
+
+let c = a = 0;
