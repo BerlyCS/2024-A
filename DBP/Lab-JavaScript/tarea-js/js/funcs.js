2bbeb08ee8a731ef323c80fddb9b9a71168fdb82
BerlyCS <berly.mandamientos@gmail.com>
2024-05-30 20:07:13 -0500

    Eliminado: Archivos innecesarios bootstrap

--- DBP/Lab-JavaScript/tarea-js/js/funcs.js
+++ DBP/Lab-JavaScript/tarea-js/js/funcs.js
@@ -1,14 +1,32 @@
+function getAge(birthday) {
+    let cumpleaños = new Date(birthday);
+    let hoy = new Date();
+    let edad = hoy.getFullYear() - cumpleaños.getFullYear();
+    let mes = hoy.getMonth() - cumpleaños.getMonth();
+    if (mes < 0) {
+        edad--;
+    }
+    return edad;
+}
+
 function escribir() {
-    let name = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
+    let nombre =  document.getElementById('nombre1').value + 
+    ' ' + document.getElementById('nombre2').value;
+    let apellido = document.getElementById('apellido1').value +
+    ' ' + document.getElementById('apellido2').value ;
     let dni = document.getElementById('dni').value;
-    let ciudad = document.getElementById('region').value;
-    console.log(name);
-    console.log(apellido);
+    let fecha = document.getElementById('fecha').value;
+
+    let nombre_completo = nombre + ' ' + apellido;
+    let edad = getAge(fecha);
+
+    console.log(nombre_completo);
     console.log(dni);
-    console.log(ciudad);
+    console.log(fecha);
     alert("...");
 
-    document.getElementById('namePlace').innerHTML = name;
-    document.getElementById('apellidoPlace').innerHTML = apellido;
+    document.getElementById('nombrePlace').innerHTML = nombre_completo;
+    document.getElementById('dniPlace').innerHTML = dni.substr(-1);
+    document.getElementById('edadPlace').innerHTML = edad;
 }
+
