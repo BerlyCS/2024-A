b53cf9df11a10e6a86c4b2c1693da5a8b95cb6e3
BerlyCS <berly.mandamientos@gmail.com>
2024-06-10 21:15:49 -0500

    Avance: Lab10 terminado

--- CC2/Lab10/1.cpp
+++ CC2/Lab10/1.cpp
@@ -5,10 +5,11 @@ DataGen.txt y guarde la informaci´on en un Vector. El archivo de texto contiene
 funci´on de intercambio (swap_ref) utilice una variable temporal para realizar el intercambio de posiciones de dos
 valores, e implemente una segunda funci´on que utilice el m´etodo move para realizar el mismo procesos*/
 
-/*3. Ejecute el c´odigo de ordenamiento con los datos de DataGen.txt y mida el tiempo de ambas funciones de
+/*3. Ejecute el c´odigo de ordenamiento con los datos de DataGen.txt y medioa el tiempo de ambas funciones de
 intercambio implementadas (chrono::high resolution clock::now();)*/
 
 /*4. En base a los resultados, determine cual de los m´etodos Swap implementados tiene mejor rendimiento.*/
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <utility>
@@ -16,11 +17,11 @@ intercambio implementadas (chrono::high resolution clock::now();)*/
 
 using namespace std;
 
-vector<vector<double>> leer() {
-    vector<vector<double>> coleccion;
+vector<vector<float>> leer() {
+    vector<vector<float>> coleccion;
     ifstream file("DataGen.txt");
     string data; int parity = 0;
-    vector<double> tmp;
+    vector<float> tmp;
     while (file>>data) {
         if (data.size() == 9) data.pop_back();
         if (parity == 2) {
@@ -45,127 +46,119 @@ void swap_ref(T& a, T& b ) {
 
 template <typename T>
 void swap_move(T& a, T& b) {
-    T tmp = move(a);
-    a = move(b);
-    b = move(tmp);
+    T tmp = std::move(a);
+    a = std::move(b);
+    b = std::move(tmp);
 }
 
-int partition(vector<vector<double>>& arr, int low, int high, int sub_index) {
-    int pivot = arr[high][sub_index];
-    int i = (low - 1);
-
-    for (int j = low; j < high; j++) {
-        if (arr[j][sub_index] <= pivot) {
-            i++;
-            swap_ref(arr[i], arr[j]);
-        }
-    }
-    swap_ref(arr[i + 1], arr[high]);
-    return (i + 1);
-}
-
-void merge_(vector<vector<double>>& arr, int left, int mid, int right, char**& grid) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    vector<vector<double>> L(n1);
-    vector<vector<double>> R(n2);
-
-    for (int i = 0; i < n1; ++i)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; ++j)
-        R[j] = arr[mid + 1 + j];
+void merge_ref(vector<vector<float>>& arr, int izq, int medio, int der, int sub_index) {
+    int n1 = medio - izq + 1;
+    int n2 = der - medio;
+    vector<vector<float>> izq_arr(n1);
+    vector<vector<float>> der_arr(n2);
+    for (int i = 0; i < n1; i++)
+        izq_arr[i] = arr[izq + i];
+    for (int i = 0; i < n2; i++)
+        der_arr[i] = arr[medio + 1 + i];
 
     int i = 0; 
     int j = 0;
-    int k = left; 
-
+    int k = izq;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            ++i;
+        if (izq_arr[i][sub_index] <= der_arr[j][sub_index]) {
+            swap_ref(arr[k], izq_arr[i++]);
         } else {
-            arr[k] = R[j];
-            ++j;
+            swap_ref(arr[k], der_arr[j++]);
         }
         ++k;
     }
-
     while (i < n1) {
-        arr[k] = L[i];
-        ++i;
-        ++k;
+        swap_ref(arr[k++], izq_arr[i++]);
     }
-
     while (j < n2) {
-        arr[k] = R[j];
-        ++j;
-        ++k;
-    }
-    
-}
-void merge(vector<vector<double>>& arr, int left, int mid, int right, int sub_index) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    vector<vector<double>> L(n1);
-    vector<vector<double>> R(n2);
-
-    for (int i = 0; i < n1; ++i)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; ++j)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0; 
-    int j = 0;
-    int k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i][sub_index] <= R[j][sub_index]) {
-            swap_ref(arr[k], L[i]);
-            ++i;
-        } else {
-            swap_ref(arr[k], R[j]);
-            ++j;
-        }
-        ++k;
-    }
-
-    while (i < n1) {
-        swap_ref(arr[k], L[i]);
-        ++i;
-        ++k;
-    }
-
-    while (j < n2) {
-        swap_ref(arr[k], R[j]);
-        ++j;
-        ++k;
+        swap_ref(arr[k++], der_arr[j++]);
     }
 }
-void merge_sort(vector<vector<double>>& array, int izq, int der, int sub_index) {
+
+void ordenar_ref(vector<vector<float>>& array, int izq, int der, int sub_index) {
     if (izq < der) {
-        int mid = izq + (der - izq) / 2;
-        merge_sort(array, izq, mid, sub_index);
-        merge_sort(array, mid + 1, der, sub_index);
-
-        merge(array, izq, mid, der, sub_index);
+        int medio = izq + (der - izq) / 2;
+        ordenar_ref(array, izq, medio, sub_index);
+        ordenar_ref(array, medio + 1, der, sub_index);
+        merge_ref(array, izq, medio, der, sub_index);
     }
 }
 
-void quickSort(vector<vector<double>>& arr, int low, int high, int sub_index) {
-    if (low < high) {
-        int pi = partition(arr, low, high, sub_index);
-        cout<<pi<<' ';
-        quickSort(arr, low, pi - 1, sub_index);
-        quickSort(arr, pi + 1, high, sub_index);
-        
+void merge_move(vector<vector<float>>& arr, int izq, int medio, int der, int sub_index) {
+    int n1=medio-izq+1;
+    int n2=der-medio;
+
+    vector<vector<float>> izq_arr(n1);
+    vector<vector<float>> der_arr(n2);
+    for (int i = 0; i < n1; i++)
+        izq_arr[i] = arr[izq + i];
+    for (int i = 0; i < n2; i++)
+        der_arr[i] = arr[medio + 1 + i];
+
+    int i = 0; 
+    int j = 0;
+    int k = izq;
+    while (i < n1 && j < n2) {
+        if (izq_arr[i][sub_index] <= der_arr[j][sub_index]) {
+            swap_move(arr[k], izq_arr[i++]);
+        } else {
+            swap_move(arr[k], der_arr[j++]);
+        }
+        ++k;
+    }
+    while (i < n1) {
+        swap_move(arr[k++], izq_arr[i++]);
+    }
+    while (j < n2) {
+        swap_move(arr[k++], der_arr[j++]);
     }
 }
+
+void ordenar_move(vector<vector<float>>& array, int izq, int der, int sub_index) {
+    if (izq < der) {
+        int medio=izq+(der-izq)/2;
+        ordenar_move(array, izq, medio, sub_index);
+        ordenar_move(array, medio + 1, der, sub_index);
+
+        merge_move(array, izq, medio, der, sub_index);
+    }
+}
+void test_con_refs() {
+    vector<vector<float>> colection = leer();
+    cout<<"Usando swap por referencia\n";
+
+    int resp = -1;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    ordenar_ref(colection, 0, colection.size()-1, 0); 
+
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop-start); 
+    cout<< resp << " por " << duration.count()/1000 << " ms" << endl;
+}
+
+void test_con_move() {
+    vector<vector<float>> colection = leer();
+    cout<<"Usando swap por std::move()\n";
+
+    int resp = -1;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    ordenar_move(colection, 0, colection.size()-1, 0); 
+
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop-start); 
+    cout<< resp << " por " << duration.count()/1000 << " ms" << endl;
+}
+
 int main() {
-    vector<vector<double>> colection = leer();
-    merge_sort(colection, 0, colection.size()-1, 0); 
-    cout<<colection[colection.size()-1][0]<<' '<<colection[0][0];
+    test_con_refs();
+    test_con_move();
 }
 
 
