2f869fcaee11a65570133efdc5ca84d2693a24b1
BerlyCS <berly.mandamientos@gmail.com>
2024-06-10 20:00:53 -0500

    Avance: quicksort

--- CC2/Lab10/1.cpp
+++ CC2/Lab10/1.cpp
@@ -1,5 +1,6 @@
 /*1. Utilizando la librer´ıa y estructura vector<T>, implemente una funci´on que lea el archivo de texto adjunto
 DataGen.txt y guarde la informaci´on en un Vector. El archivo de texto contiene n´umeros flotantes [x, y]*/
+
 /*2. Implemente una funci´on de ordenamiento de las coordenadas (sean los valores x o los valores y) donde la
 funci´on de intercambio (swap) utilice una variable temporal para realizar el intercambio de posiciones de dos
 valores, e implemente una segunda funci´on que utilice el m´etodo std::move para realizar el mismo procesos*/
@@ -10,11 +11,10 @@ intercambio implementadas (std::chrono::high resolution clock::now();)*/
 /*4. En base a los resultados, determine cual de los m´etodos Swap implementados tiene mejor rendimiento.*/
 #include <fstream>
 #include <iostream>
-#include <ostream>
+#include <utility>
 #include <vector>
 
 using namespace std;
-
 
 vector<vector<double>> leer() {
     vector<vector<double>> coleccion;
@@ -32,39 +32,52 @@ vector<vector<double>> leer() {
         parity++;
     }
     coleccion.push_back( tmp );
+    cout<<"Datos leidos.\n";
     return coleccion;
 }
 
 template <typename T>
-void invertir(T& a, T& b ) {
+void swap_ref(T& a, T& b ) {
     T tmp = a;
     a = b;
     b = tmp;
 }
-void ord_selection(vector<vector<double>>& array, int sub_index){
-    int pointer,max,
-        num = array.size(),
-        count = 0;
 
-    for (int i=num;i>=0;i--) {
-        if (count == 10000) {
-            cout<<i<<' '<<flush;
-            count=0;
-        }
-        max=0;
-        for (int j=0;j<i;j++) {
-            if (array[max][sub_index]<array[j][sub_index]) {
-                max=j;
-            }
-        }
-        invertir(array[i-1],array[max]);
-        count++;
-    }
+template <typename T>
+void swap_move(T& a, T& b) {
+    T tmp = std::move(a);
+    a = std::move(b);
+    b = std::move(tmp);
 }
 
+int partition(vector<vector<double>>& arr, int low, int high, int sub_index) {
+    int pivot = arr[high][sub_index];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j][sub_index] <= pivot) {
+            i++;
+            swap_ref(arr[i], arr[j]);
+        }
+    }
+    swap_ref(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+
+void quickSort(vector<vector<double>>& arr, int low, int high, int sub_index) {
+    if (low < high) {
+        int pi = partition(arr, low, high, sub_index);
+        cout<<pi<<' ';
+        quickSort(arr, low, pi - 1, sub_index);
+        quickSort(arr, pi + 1, high, sub_index);
+        
+    }
+}
 int main() {
     vector<vector<double>> colection = leer();
-    ord_selection(colection, 0); 
+    quickSort(colection, 0, colection.size()-1, 0); 
+    cout<<colection[colection.size()-1][0]<<' '<<colection[0][0];
 }
 
 
